6cdf61427233d07996e15fcc72e455ebcc9c6297
AbdellaouiBen <benabdellaoui22@gmail.com>
2022-02-27 11:53:26 +0100

    react native todo list

---
+++ App.js
@@ -0,0 +1,107 @@
+import {
+  Keyboard,
+  KeyboardAvoidingView,
+  Platform,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View
+} from 'react-native';
+import Task from "./components/Task";
+import {useState} from "react";
+
+export default function App() {
+  const [task, setTask] = useState();
+  const [taskItems, setTaskItems] = useState([]);
+  const handleAddTask = () =>{
+    Keyboard.dismiss();
+    setTaskItems((prevValue)=>[...prevValue, task] );
+    setTask(null);
+  };
+  const completeTask = (index) =>{
+    const itemsCopy = [...taskItems];
+    itemsCopy.splice(index,1);
+    setTaskItems(itemsCopy);
+  };
+
+  return (
+    <View style={styles.container}>
+      {/*Today's Tasks*/}
+      <View style={styles.taskWrapper}>
+        <Text style={styles.sectionTitle}>Pomme de pain</Text>
+         <View style={styles.items}>
+           {/*This is*/}
+           {taskItems.map((item, index)=> {
+             return (<TouchableOpacity key={index}  onPress={()=> completeTask(index)}>
+               <Task text={item}/>
+             </TouchableOpacity>)
+           })}
+         </View>
+      </View>
+
+
+      {/*Write a Tasks*/}
+      <KeyboardAvoidingView
+        style={styles.writeTaskWrapper}
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+      >
+        <TextInput  style={styles.input} placeholder={"Write a task"} value={task} onChangeText={text=>setTask(text)} />
+
+        <TouchableOpacity onPress={()=>handleAddTask()} >
+          <View style={styles.addWrapper}>
+            <Text  style={styles.addText} >+</Text>
+          </View>
+        </TouchableOpacity>
+
+      </KeyboardAvoidingView >
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#E8EAED",
+  },
+  taskWrapper: {
+    paddingTop: 80,
+    paddingHorizontal: 20,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: "bold",
+    paddingVertical: 10,
+  },
+  items: {
+    paddingVertical: 10,
+  },
+  writeTaskWrapper: {
+    position: "absolute",
+    bottom: 60,
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
+  },
+  input: {
+    paddingVertical: 15,
+    paddingHorizontal: 15,
+    backgroundColor: "#fff",
+    borderRadius: 60,
+    borderColor: "#C0C0C0",
+    borderWidth: 1,
+    width: 250,
+  },
+  addWrapper: {
+    width: 60,
+    height: 60,
+    backgroundColor: "#fff",
+    borderRadius: 60,
+    justifyContent: "center",
+    alignItems: "center",
+    borderWidth: 1,
+  },
+  addText: {
+  }
+});
---
+++ README.md
@@ -0,0 +1,5 @@
+# Todo List - React Native
+done with [youtube](https://www.youtube.com/playlist?list=PLYBvEAka-q1hJuwRPYQPlEBBRm7_qGw_2) by [Made with Matt](https://www.youtube.com/c/MadeWithMatt)
+
+
+
